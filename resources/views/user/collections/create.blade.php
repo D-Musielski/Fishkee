2f59aecfbcd0250c299c6e0db5d7525df612e194
Dominik Musielski <dominik.musielski@gmail.com>
2017-12-20 00:05:01 +0100

    Added 'required' to collection name field

--- resources/views/user/collections/create.blade.php
+++ resources/views/user/collections/create.blade.php
@@ -8,7 +8,7 @@
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="name">Nazwa zbioru</label>
-                <input type="text" name="name" class="form-control">
+                <input type="text" name="name" class="form-control" required>
             </div>
             <div class="form-group">
                 <button class="btn btn-success" type="submit">Utwórz zbiór</button>
