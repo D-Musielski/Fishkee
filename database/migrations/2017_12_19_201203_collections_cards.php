f56a088fc3eca99ffee6c802bedb93ff41c0b6c2
Dominik Musielski <dominik.musielski@gmail.com>
2017-12-19 21:14:12 +0100

    collections_cards migration

---
+++ database/migrations/2017_12_19_201203_collections_cards.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CollectionsCards extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections_cards', function (Blueprint $table) {
+            $table->integer('collection_id')->unsigned();
+            $table->integer('card_id')->unsigned();
+            $table->primary(['collection_id','card_id']);
+            $table->foreign('collection_id')->references('id')->on('collections')->onDelete('CASCADE');
+            $table->foreign('card_id')->references('id')->on('cards')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
