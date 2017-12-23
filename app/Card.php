963b04857b5e3102eecbd1bfbe8f13962eeb7933
Dominik Musielski <dominik.musielski@gmail.com>
2017-12-23 21:10:57 +0100

    Added fillable for Card

--- app/Card.php
+++ app/Card.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
+    protected $fillable = [
+        'front', 'back'
+    ];
+
     public function collections()
     {
         return $this->belongsToMany('App\Collection');
