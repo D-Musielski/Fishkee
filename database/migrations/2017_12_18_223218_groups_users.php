ea9352bc5073d856e06b93b267ecc1e722775d98
Dominik Musielski <dominik.musielski@gmail.com>
2018-01-08 20:21:35 +0100

    Renamed table

--- database/migrations/2017_12_18_223218_groups_users.php
+++ database/migrations/2017_12_18_223218_groups_users.php
@@ -14,7 +14,7 @@ class GroupsUsers extends Migration
     public function up()
     {
         
-        Schema::create('groups_users', function (Blueprint $table) {
+        Schema::create('group_user', function (Blueprint $table) {
             $table->integer('group_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->primary(['group_id','user_id']);
@@ -30,6 +30,6 @@ class GroupsUsers extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('groups_users');
+        Schema::dropIfExists('group_user');
     }
 }
